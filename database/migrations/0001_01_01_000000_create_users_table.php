f2a3f37155991046e95f05479db00c2c93b2d17b
Alexander Nacho <nachoalexander36@gmail.com>
2026-05-26 23:41:04 -0400

    Fix users migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -6,30 +6,28 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
 
-            // 👤 Nombre completo del usuario
             $table->string('name');
-
-            // 🔑 Usuario (LOGIN PRINCIPAL)
             $table->string('username')->unique();
 
-            // 🔒 Contraseña encriptada
             $table->string('password');
 
-            // 🎭 Rol del sistema (admin o user)
-            $table->enum('role', ['admin', 'user'])->default('user');
-
-            // ⏱ Token para "recordarme"
-            $table->rememberToken();
+            $table->string('role')->default('user');
 
             $table->timestamps();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::dropIfExists('users');
